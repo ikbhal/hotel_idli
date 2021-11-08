86bdfeaeb259aaa1f8fbb13766763e7857ce0cab
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2021-11-08 12:31:35 +0530

    add steps

--- src/App.svelte
+++ src/App.svelte
@@ -26,7 +26,13 @@
 		<li>Wash Idli rice ravva </li>
 		<li>soak Idli rice ravva for 4 to 6 hours</li>
 		<li>Flattened rice for 1 hour</li>
-		
+		<li>Grind daal </li>
+		<li>Add flattend rice while grinding- remove water while adding</li>
+		<li>add idli rice ravva - remove water, press if neeeded -note: no grinding neede</li>
+		<li>MIx with your hand for 2 or 3 minutes</li>
+		<li>Note: mixie will make daal heat, for mix chilled water, one speed, take break, mix with hand later</li>
+		<li>Wet grinder: add water first to wet rock, use spoon to control flow , will get consistancy, fluffy</li>
+
 	</ul>
 
 </main>
