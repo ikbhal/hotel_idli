e6ff17f7b8bf28f2460c34977788f4e578479164
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2021-11-08 12:21:25 +0530

    first commit  hotel idli

---
+++ src/App.svelte
@@ -0,0 +1,35 @@
+<script>
+</script>
+
+
+<main>
+
+	<h1>Hotel Idli</h1>
+	<iframe width="560" height="315" src="https://www.youtube.com/embed/ukC79WIL6-o" 
+		title="YouTube video player" frameborder="0" 
+		allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+		 allowfullscreen>
+	</iframe>
+    
+	<h3>Ingredients</h3>
+	<ul>
+		<li>Skinless Whole Black Gram - 1 cup </li>
+		<li>Idli Rice Ravva - 3 cups</li>
+		<li>Poha / Flattened Rice - 1 cup</li>
+	</ul>
+
+	<h3>Steps</h3>
+	<ul>
+		<li>Wash daal</li>
+		<li>soak daal for 4 to 6 hours</li>
+		<li>add 1/2 tsp Fenu greek seeds</li>
+		<li>Wash Idli rice ravva </li>
+		<li>soak Idli rice ravva for 4 to 6 hours</li>
+		<li>Flattened rice for 1 hour</li>
+		
+	</ul>
+
+</main>
+
+<style>
+</style>
